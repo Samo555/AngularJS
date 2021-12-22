d1c99b5189e3279deb2970c883644eed9bd4c046
Samo555 <samvel_sar19@mail.ru>
2021-12-22 15:31:03 +0400

    gitignore update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-first'`, () => {
+  it(`should have as title 'gitc-group-2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-first');
+    expect(app.title).toEqual('gitc-group-2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('my-first app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('gitc-group-2 app is running!');
   });
 });
